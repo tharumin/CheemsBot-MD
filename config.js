741d3db97b63c35121d6043cfc78fc9ffa43398b
tharumin <94857655+tharumin@users.noreply.github.com>
2022-04-11 22:35:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
-global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
-global.ownernma = '🦄Dream Guy Xeon' //owner name
-global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.owner = ['94704247900','94704421161'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['94704247900'] //another owner number
+global.premium = ['94704247900'] //premium number
+global.pengguna = 'ᴍ.ʀ sͥᴀͣᴅͫᴇᴇsʜᴀ🧑🏻‍💻' //username
+global.botnma = 'Dog-bot sadiya' //bot name
+global.ownernma = '🦄Dream Guy sadiya' //owner name
+global.packname = 'sadiya Bot' //sticker package name
+global.author = 'By Dream Guy sadiya' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
